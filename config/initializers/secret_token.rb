1fe02c04b04580465bfea7db84e5b1afd7349d4f
Rahul Trikha <rahul.trikha@gmail.com>
2012-03-29 17:06:14 +1030

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MeraLakshya::Application.config.secret_token = '43100dff26a9e7fbcf53027987a26dca3a4f32fcf78a7ef069a8fef7874020ffb1cf365f6e2b6a5836d06f75303b73c60cf43f29f786b050a1d807f55f5a74cf'
